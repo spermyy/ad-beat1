2dd941e933bc27337788b288dd4b994c6b8ca811
spermyy <manarangdaniel@gmail.com>
2024-08-27 12:11:01 +0800

    second commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,20 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello I am Daniel Manarang I am a Software Engineer]</p>
+        <p>[Hello I am Daniel Manarang I am a Software Engineer I’m Daniel Manarang, I live in Macabebe, Pampanga. My hobbies are playing basketball and mobile games and cooking. My favorite food is Filipino food like Lumpiang Sariwa And Ginisang Ampalaya. I like a lot of vegetable food. Other things that I can tell you about me is I like watching movies/series and listening to music.
+
+
+I took up IT because my sister told me to take the IT course.
+
+
+After graduation I see myself as a Software Developer Or Maybe other work not related to my course.
+
+
+Integration Strategies Understanding various approaches to integrating systems, such as point-to-point, middleware, and service-oriented architecture and,
+Process Modeling: Documenting and analyzing business processes to improve efficiency and integration.
+
+Integration Techniques and Strategies, Architectural Frameworks and Model and Data Integration and Management and more.
+]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
